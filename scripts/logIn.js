7609f3d787e81eac0e7d069dba2b1c58513c3395
Mike <michel.a.vieira98@gmail.com>
2025-04-15 19:49:24 +0200

    Login script and erro msg added

---
+++ scripts/logIn.js
@@ -0,0 +1,29 @@
+let users = [];
+const BASE_URL = 'https://join-gruppenarbeit-a540b-default-rtdb.europe-west1.firebasedatabase.app';
+
+async function getAllUseers() {
+    let response = await fetch(`${BASE_URL}/users.json`);
+    let data = await response.json();
+
+    users = data
+}
+
+function logIn() {
+    let email = document.getElementById('emailLogIn').value;
+    let password = document.getElementById('passwordLogIn').value;
+
+    const user = Object.values(users).find(u => u.email === email && u.password === password);
+
+    if (user) {
+        window.location.href = 'pages/summary.html';
+    } else {
+        document.getElementById("errorSignUp").classList.remove("d_none");
+        document.getElementById("errorSignUp").textContent = "Check your email and password. Please try again.";
+    }
+}
+
+function logInGuest() {
+    window.location.href = 'pages/summary.html';
+}
+
+getAllUseers();
