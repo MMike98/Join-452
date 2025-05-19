30f46c7cce57d7e4f428d781932cd4ddfd1bb4e5
f-michaelsen <finnm@hotmail.de>
2025-05-19 20:44:07 +0200

    added navbar javascript

--- scripts/template.js
+++ scripts/template.js
@@ -1,5 +1,40 @@
 // SCRIPT FOR NAVIGATION BAR AND HEADER //*css*/`
-    
-function buttonActive() {
+/**
+ * 
+ * @param {*click} event 
+ */
+function buttonActive(event) {
+  event.preventDefault();
 
+  const navLinks = document.querySelectorAll(".nav-bar-button");
+
+  for (let i = 0; i < navLinks.length; i++) {
+    navLinks[i].classList.remove("active");
+  }
+
+  event.target.classList.add("active");
 }
+
+const navLinks = document.querySelectorAll(".nav-bar-button");
+for (let i = 0; i < navLinks.length; i++) {
+  navLinks[i].addEventListener("click", buttonActive);
+}
+
+/**
+ * 
+ */
+function handleDOMContentLoaded() {
+  const navLinks = document.querySelectorAll('.nav-bar-button');
+  const currentPage = window.location.pathname.split('/').pop();
+
+  for (let i = 0; i < navLinks.length; i++) {
+    const href = navLinks[i].getAttribute('href');
+    const hrefPage = href.split('/').pop();
+
+    if (currentPage === hrefPage) {
+      navLinks[i].classList.add('active');
+    }
+  }
+}
+
+window.addEventListener('DOMContentLoaded', handleDOMContentLoaded);
